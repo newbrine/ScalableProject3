8bc279a64ae8562eabb78cb10b7d1ea9af2066a1
newbrine <35578361+newbrine@users.noreply.github.com>
2018-03-26 21:46:05 -0500

    Add files via upload

---
+++ src/GUI/GUIController.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class GUIController {
+
+	@FXML
+	public void initialize() {
+		
+	}
+	
+	@FXML
+	public void addNewPatientDonor() {
+		GUIPopups popups = new GUIPopups();
+		popups.AddPatientDonor();
+	}
+	
+	@FXML
+	public void deletePatientDonor() {
+		GUIPopups popups = new GUIPopups();
+		popups.DeletePatientDonor();
+	}
+	
+	@FXML
+	public void searchPatientDonor() {
+		GUIPopups popups = new GUIPopups();
+		popups.SearchPatientDonor();
+	}
+}
