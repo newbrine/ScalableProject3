10a4798042f8814619e9942e288bc7a7dd03c7e5
newbrine <35578361+newbrine@users.noreply.github.com>
2018-04-01 22:58:21 -0500

    Update Donor.java

--- src/people/Donor.java
+++ src/people/Donor.java
@@ -19,7 +19,7 @@ public class Donor implements People {
 
 	@Override
 	public void remove(String id) {
-		Database.readCommand("DELETE FROM Donor WHERE Id = \' " + id + " '");
+		Database.readCommand("DELETE FROM Donor WHERE Id = '" + id + "'");
 		try {
 			Database.printAll("Donor");
 		} catch (SQLException e) {
