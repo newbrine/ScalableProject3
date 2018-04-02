529a2ab7941fbd06290a74ab51b779b8fdeff82a
Unknown <samantha.wycoff1@yahoo.com>
2018-04-01 22:17:16 -0500

    Import Cleanup

--- src/GUI/GUIController.java
+++ src/GUI/GUIController.java
@@ -1,17 +1,8 @@
 package GUI;
 
-import java.util.ArrayList;
-
 import people.Patient;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 public class GUIController {
 	
