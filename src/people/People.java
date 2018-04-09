cc7ca72c5330030027cb89e5aa202cb3f8465cb4
newbrine <35578361+newbrine@users.noreply.github.com>
2018-04-08 22:43:57 -0500

    Delete People.java

--- src/people/People.java
+++
@@ -1,9 +0,0 @@
-package people;
-
-import java.sql.SQLException;
-
-public interface People {
-	public void add(String name, String id, String bloodtype, String organ) throws SQLException;
-	
-	public void remove(String id);
-}
